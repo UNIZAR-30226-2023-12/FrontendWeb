b3506a59d80d7bc520aaacb50ec1216e92d744b2
Ismael-V <ismaelpsabi@gmail.com>
2023-04-14 14:16:31 +0200

    Co-authored-by: Paula Olivan <PaulaOlivan@users.noreply.github.com>

--- melodia/src/App.js
+++ melodia/src/App.js
@@ -1,24 +1,61 @@
+
 import logo from './logo.svg';
-import './App.css';
+import './Style.css';
+import { useState } from 'react';
+import { Button, Toast } from 'react-bootstrap';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+function CrearMensaje(){
+  const [mensajeToast, setMensajeToast] = useState('');
+}
+
+/*
+const mostrarToast = (mensaje) => {
+  setMensajeToast(mensaje);
+  setTimeout(() => {
+    setMensajeToast('');
+  }, 3000); // el toast se ocultará después de 3 segundos
+};
+
+const manejarClickBoton = (mensaje) => {
+  mostrarToast(`Me has pulsado: ${mensaje}`);
+};
+*/
+
+function fav(){
+  toast.success("Cancion añadida a favoritos")
+}
+
+function Menu() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+    toast.success("¡Menú abierto!"); // Mostrar toast cuando se abra el menú
+  }
+
+  return (
+    <div className="menu">
+      <button onClick={toggleMenu}>Menú</button>
+      {isOpen && (
+        <ul>
+          <li><a href="#" onClick={fav}>Opción 1</a></li>
+          <li><a href="#">Opción 2</a></li>
+          <li><a href="#">Opción 3</a></li>
+        </ul>
+      )}
+    <ToastContainer/>
+    </div>
+  );
+}
 
 function App() {
+
+  CrearMensaje();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Menu/>
   );
 }
 
