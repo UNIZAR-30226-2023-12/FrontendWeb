bb146c476f007cdf5805369a1968081769353e57
Ismael-V <ismaelpsabi@gmail.com>
2023-04-19 12:57:36 +0200

    Co-authored-by: Paula Olivan <PaulaOlivan@users.noreply.github.com>

--- melodia/src/App.js
+++ melodia/src/App.js
@@ -1,8 +1,9 @@
 
 import './Style.css';
-import React from 'react';
+import React, { useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import styled from "styled-components"
 
 import { createRoot } from 'react-dom/client';
 
@@ -12,7 +13,7 @@ const root = createRoot(domNode);
 class BarraNavegacion extends React.Component{
   render(){
     return (
-      <nav class="navbar navbar-expand-lg navbar-dark bg-blue_4th">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-blue_4th" style={{position : 'fixed', width : '100%'}}>
               <div class="container px-5">
                   <a class="navbar-brand" href="#!">Melodia</a>
                   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
@@ -21,6 +22,28 @@ class BarraNavegacion extends React.Component{
                           <li class="nav-item"><a class="nav-link active" aria-current="page" onClick={principal}>Inicio</a></li>
                           <li class="nav-item"><a class="nav-link" onClick={inicioSesion}>Iniciar Sesión</a></li>
                           <li class="nav-item"><a class="nav-link" onClick={registrarse}>Registrarse</a></li>
+                      </ul>
+                  </div>
+              </div>
+      </nav>
+    )
+  } 
+}
+
+class BarraNavegacionApp extends React.Component{
+  render(){
+    return (
+      <nav class="navbar navbar-expand-lg navbar-dark bg-blue_4th">
+              <div class="container px-5">
+                  <a class="navbar-brand" href="#!">Melodia</a>
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                          <li class="nav-item"><a class="nav-link active" aria-current="page">Notificaciones  &#128276;</a></li>
+                          <li class="nav-item"><a class="nav-link">Perfil &#128578;	</a></li>
+                          <li class="nav-item"><a class="nav-link">Top Diario &#129351; </a></li>
+                          <li><p>&emsp;&emsp;&emsp;&emsp;</p></li>
+                          <li class="nav-item"><a class="nav-link" onClick={inicioSesion}>Cerrar Sesión &#128682;</a></li>
                       </ul>
                   </div>
               </div>
@@ -53,12 +76,6 @@ class Cabecera extends React.Component{
 }
 
 class ImagenInfo extends React.Component{
-  /*
-    constructor(props){
-      super(props)
-    }
-  */
-
   render(){
     return(
       <div class="col-lg-6 col-xl-4">
@@ -83,15 +100,15 @@ class FormularioInicio extends React.Component{
   render(){
     return(
         <header class="bg-blue_7th py-5">
-            <div class="container px-5">
+            <div class="container px-5" style={{"margin-top" : "8%"}}>
                 <div class="row gx-5 justify-content-center">
                     <div class="col-lg-6">
                         <div class="text-center my-5">
                             <h1 class="display-5 fw-bolder text-white mb-2" style={{"padding-bottom" : "1 rem"}}>Iniciar Sesión</h1>
-                                <form id="contactForm" style={{"margin" : "auto", "max-width" : "20rem", "width" : "100%", "align-self": "center"}}>
+                                <form id="contactForm" style={{"margin" : "auto", "max-width" : "20rem", "width" : "100%", "align-self": "center"}} onSubmit={menuPrincipal}>
                                     <div class="form-floating mb-3">
-                                        <input class="form-control" id="name" type="text" placeholder="Introduce tu nombre de Usuario"/>
-                                        <label for="name">Usuario</label>
+                                        <input class="form-control" id="name" type="text" placeholder="Introduce tu correo electronico"/>
+                                        <label for="name">Email</label>
                                     </div>
                                     <div class="form-floating mb-3">
                                         <input class="form-control" id="passwd" type="password" placeholder="Introduce tu Contraseña"/>
@@ -121,7 +138,7 @@ class FormularioRegistro extends React.Component {
                       <div class="col-lg-6">
                           <div class="text-center my-5">
                               <h1 class="display-5 fw-bolder text-white mb-2" style={{"padding-bottom" : "1rem"}}>Registrarse</h1>
-                                  <form id="contactForm" style={{"margin": "auto", "max-width": "20rem", "width" : "100%", "align-self" : "center"}} onSubmit={comprobar_entrada}action="Prueba">
+                                  <form id="contactForm" style={{"margin": "auto", "max-width": "20rem", "width" : "100%", "align-self" : "center"}} onSubmit={comprobar_entrada_registro}action="Prueba">
                                       <div class="form-floating mb-3">
                                           <input class="form-control" id="email" type="text" placeholder="ejemplo@gmail.com"/>
                                           <label for="email">Correo electrónico</label>
@@ -153,12 +170,58 @@ class FormularioRegistro extends React.Component {
   }
 }
 
+class Button extends React.Component{
+  render(){
+    return (<button class="btn btn-primary_blue_4th btn-lg px-4 me-sm-3" id={this.props.id} style={this.props.style}>{this.props.text}</button>)
+  }
+}
+
+const ButtonGroup = styled.div`
+  display: flex;
+  justify-content: center;
+`
+// Usage
+
+const AudioPlayer = () => {
+  return (
+    <div className="audio-player">
+      <div className="inner">Audio player content</div>
+    </div>
+  );
+};
+
+class MenuPrincipal extends React.Component{
+  render(){
+    return(
+      <>
+        <div class="form-floating mb-3">
+          <input class="form-control" id="busqueda" type="text" placeholder="Cancion X"/>
+          <label for="busqueda">Buscar  &#128269;</label>
+        </div>
+        {ultimo_punto_de_escucha()}
+        <p></p>
+        <ButtonGroup>
+          <Button id="" text="Mis Carpetas"/>
+          <Button id="" text="Mis Listas"/>
+        </ButtonGroup>
+        <p></p>
+        <ButtonGroup>
+          <Button id="" text="Favoritos"/>
+          <Button id="" text="Random"/>
+          <Button id="" text="Social"/>
+        </ButtonGroup>
+        <p></p>
+      </>
+    )
+  }
+}
+
 class Footer extends React.Component{
   render(){
     return (
         <>
-          <footer class="py-5 bg-blue_4th">
-              <div class="container px-5"><p class="m-0 text-center text-white">Copyright &copy; Mussa Enterprise 2023</p></div>
+          <footer style={{position: 'fixed', width : '100%', bottom: 0}} class="py-5 bg-blue_4th">
+              <div class="container px-5"><p class="m-0 text-center text-white">Copyright &copy; Mussa Enterprise&#xae; 2023</p></div>
           </footer>
         </>
     )
@@ -193,7 +256,7 @@ function Inicio() {
   );
 }
 
-function comprobar_entrada(e){
+function comprobar_entrada_registro(e){
   /* Sean los campos del formulario rellenado */
   let email = (document.getElementById("email")).value
   let usuario = (document.getElementById("name")).value
@@ -230,6 +293,34 @@ function enviar_peticion_registro(email, usuario, passwd){
   .then(response => console.log('Success:', response));
 }
 
+function enviar_peticion_inicio(email, passwd){
+  fetch("http://192.168.56.1:8081/ValidateUser/", {
+    method : "POST",
+    body : JSON.stringify({"email" : email, "contrasenya" : passwd})
+  }).then(res => res.json())
+  .catch(error => console.error('Error:', error))
+  .then(response => console.log('Success:', response));
+}
+
+function ultimo_punto_de_escucha(){
+
+  //TODO: Comprobar que haya un ultimo punto de escucha
+  return(
+    <ButtonGroup>
+          <Button id="" text="Continuar ultima escucha"/>
+    </ButtonGroup>
+  )
+}
+
+/*function listarListaReproduccion(idUsuario, ){
+  fetch("http://192.168.56.1:8081/GetListasUsr/", {
+    method : "POST",
+    body : JSON.stringify({"idUsr" : 0, "email" : email, "contrasenya" : passwd, "tipoUsuario" : "normalUser", "alias" : usuario})
+  }).then(res => res.json())
+  .catch(error => console.error('Error:', error))
+  .then(response => console.log('Success:', response));
+}*/
+
 function Login(){
   return (
     <div className="menu">
@@ -252,6 +343,17 @@ function Signin(){
   );
 }
 
+function Menu(){
+  return(
+    <div className="menu">
+      <BarraNavegacionApp/>
+      <MenuPrincipal/>
+      <Footer/>
+      <ToastContainer/>
+    </div>
+  )
+}
+
 function App() {
 
   return (
@@ -270,6 +372,10 @@ function inicioSesion(){
 
 function registrarse(){
   root.render(<Signin/>, document.getElementById('root'))
+}
+
+function menuPrincipal(){
+  root.render(<Menu/>, document.getElementById('root'))
 }
 
 export default App;
